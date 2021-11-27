0cd6155f39d00c5e397eb97fe877d37820472dbb
shake <hiroki.yamako@ezweb.ne.jp>
2021-11-27 16:41:22 +0900

    [add]テーブル作成

--- opt/delivery/models.py
+++ opt/delivery/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Bus(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class BusStop(models.Model):
+    name = models.CharField(max_length=255)
+    time = models.DateTimeField()
+    bus = models.ForeignKey(Bus, on_delete=models.CASCADE)
+
+
+class CarryBus(models.Model):
+    bus = models.ForeignKey(Bus, on_delete= models.CASCADE)
+    start_station = models.CharField(max_length=255)
+    end_station = models.CharField(max_length=255)
+    max_size = models.IntegerField()
+
+
+class Reservation(models.Model):
+    carry_bus = models.ForeignKey(CarryBus, on_delete=models.CASCADE)
+    ride_flag = models.BooleanField()
+    carry_flag = models.BooleanField()
+    fee = models.IntegerField()
